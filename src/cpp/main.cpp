ebbaa20068fb8d3d7e63f355d5fab6c3fc814f2b
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2018-10-09 13:20:32 +0200

    add directory batching

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -5,6 +5,9 @@
 #include "PMUTAlgorithm.hpp"
 #include "UnifAlgorithm.hpp"
 
+#include <algorithm>
+//#include <experimental/filesystem>
+#include <filesystem>
 #include <iostream>
 #include <memory>
 #include <string>
@@ -13,18 +16,34 @@
 using namespace maxcut;
 using namespace std;
 
+namespace {
+
+struct path_leaf_string {
+  std::string operator()(const std::filesystem::directory_entry &entry) const {
+    return entry.path().string();
+  }
+};
+
+vector<string> read_directory(const std::string &name) {
+  vector<string> v;
+  std::filesystem::path p(name);
+  std::filesystem::directory_iterator start(p);
+  std::filesystem::directory_iterator end;
+  std::transform(start, end, std::back_inserter(v), path_leaf_string());
+  return v;
+}
+
+} // namespace
+
 int main() {
-  vector<string> filenames{
-      "mtx/mvccographsbip.n100m30p0.03.1.mtx",
-      "mtx/mvccographsbip.n100m30p1.0.1.mtx",
-      //"mtx/ca-citeseer.mtx", "ca-dblp-2010.mtx",
-      //"web-edu.mtx"
-  };
+  string dirname = R"(/home/tbjoern/development/maxcut/maxcutevo/build/graphs)";
   vector<unique_ptr<Algorithm>> algorithms;
 
   algorithms.push_back(make_unique<UnifAlgorithm>());
   // algorithms.push_back(make_unique<AnnealingAlgorithm>());
   algorithms.push_back(make_unique<PMUTAlgorithm>());
+
+  auto filenames = read_directory(dirname);
 
   Benchmark benchmark(filenames, algorithms);
   auto results = benchmark.run();
