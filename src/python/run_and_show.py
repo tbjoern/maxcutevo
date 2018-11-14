889d82e8013a4a79e72acb32af735ece22eb0907
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2018-11-14 15:41:31 +0100

    add run and show script

---
+++ src/python/run_and_show.py
@@ -0,0 +1,18 @@
+from batch import Batch
+from plotter import Plotter
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 3:
+        print("Usage: run_and_show.py <config_file> [instance_file, ...]")
+        exit(127)
+    
+    from config_reader import read_config
+
+    batch = Batch(read_config(sys.argv[1]))
+    for filename in sys.argv[2:]:
+        logfile = batch.run(filename)
+        plotter = Plotter(logfile)
+        plotter.prepare_plot()
+        plotter.show_plot()
+
