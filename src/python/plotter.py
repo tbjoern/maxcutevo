6cf9ea1cbc997b7d1fff7c280615b4d545c50c56
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2018-11-15 15:09:50 +0100

    change plotter to show mean and std dev

--- src/python/plotter.py
+++ src/python/plotter.py
@@ -1,5 +1,6 @@
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
 class Plotter:
     def __init__(self, csv_name, mapping_name=None):
@@ -13,17 +14,29 @@ class Plotter:
             for row in csvreader:
                 algo_id = int(row["id"])
                 self.labels[algo_id] = row["name"]
-                self.data[algo_id] = { 'iteration':[], 'cut_weight':[] }
+                self.data[algo_id] = {}
         with open(csv_name, "r") as f:
             csvreader = csv.DictReader(f, delimiter=',')
             for row in csvreader:
                 algo_id = int(row["algorithm"])
-                self.data[algo_id]['iteration'].append(int(row['iteration']))
-                self.data[algo_id]['cut_weight'].append(int(row['cut_weight']))
+                run_nr = int(row["run_number"])
+                if not run_nr in self.data[algo_id]:
+                    self.data[algo_id][run_nr] = { 'iteration':[], 'cut_weight':[] }
+                self.data[algo_id][run_nr]['iteration'].append(int(row['iteration']))
+                self.data[algo_id][run_nr]['cut_weight'].append(int(row['cut_weight']))
 
     def prepare_plot(self):
-        for algo_id, data in self.data.items():
-            plt.plot(data['iteration'], data['cut_weight'], self.colors[algo_id%len(self.colors)] + '-', label=self.labels[algo_id])
+        for algo_id, runs in self.data.items():
+            data_array = []
+            for run_nr, data in runs.items():
+                data_array.append(data['cut_weight'])
+            nparray = np.array(data_array)
+            cut_weight_mean = nparray.mean(axis=0)
+            sigma = nparray.std(axis=0)
+            color = self.colors[algo_id%len(self.colors)]
+            fmt = color + '-'
+            plt.plot(data['iteration'], cut_weight_mean, fmt, label=self.labels[algo_id])
+            plt.fill_between(data['iteration'], cut_weight_mean+sigma, cut_weight_mean-sigma, facecolor=color, alpha=0.5)
         plt.legend(loc='lower right')
 
     def show_plot(self):
