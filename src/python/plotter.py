cc5e3d5d6fbcd9801d3608d455cb5489a22c6e65
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2018-12-17 16:17:27 +0100

    plotter: fix parsing of incomplete runs

--- src/python/plotter.py
+++ src/python/plotter.py
@@ -58,13 +58,20 @@ class Plotter:
             data_array = []
             for run_nr, run_data in runs.items():
                 data_array.append(run_data['cut_weight'])
+            min_length = None
+            for run_data in data_array:
+                if min_length is None or len(run_data) < min_length:
+                    min_length = len(run_data)
+            for i, run_data in enumerate(data_array):
+                data_array[i] = run_data[:min_length]
             nparray = np.array(data_array)
             cut_weight_mean = nparray.mean(axis=0)
             sigma = nparray.std(axis=0)
             color = self.colors[algo_id%len(self.colors)]
             fmt = color + '-'
-            plt.plot(runs[0]['iteration'], cut_weight_mean, fmt, label=labels[algo_id])
-            plt.fill_between(runs[0]['iteration'], cut_weight_mean+sigma, cut_weight_mean-sigma, facecolor=color, alpha=0.5)
+            indices = np.arange(min_length)
+            plt.plot(indices, cut_weight_mean, fmt, label=labels[algo_id])
+            plt.fill_between(indices, cut_weight_mean+sigma, cut_weight_mean-sigma, facecolor=color, alpha=0.5)
         plt.legend(loc='lower right')
         plt.suptitle(file)
 
