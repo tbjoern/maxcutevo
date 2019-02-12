9a68e8dcfb24cc15207f350e905ebb2aafdc53d3
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2019-02-12 15:38:49 +0100

    update batcher script for usage in build folder

--- src/script/batcher.sh
+++ src/script/batcher.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 dir=$1
-exec_name='build/maxcut-benchmark'
-config_file='build/config.json'
+exec_name='./maxcut-benchmark'
+config_file='./config.json'
 
 echo $2
 install -d $2
 
-shopt -s globstar
-
-for file in $dir/**/*.*
+for file in `find -L $dir -type f`
 do
     echo "${exec_name} ${file} ${config_file} > ${2}/${file}"
-    echo ${2}/$file
-    install -Dv -m 644 /dev/null "${2}/$file"
+    echo "${2}/${file}"
+    install -Dv -m 644 /dev/null "${2}/${file}"
 	eval "${exec_name} ${file} ${config_file} > ${2}/${file}"
 done
