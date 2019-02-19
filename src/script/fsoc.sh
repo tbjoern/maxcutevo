d0ec8c89af541fd23c3f45701b70290ab3f677b8
Soeren Tietboehl <soeren.tietboehl@student.hpi.uni-potsdam.de>
2019-02-19 15:30:17 +0100

    update fsoc script

--- src/script/fsoc.sh
+++ src/script/fsoc.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name maxcut-benchmark
-#SBATCH --ntasks 40
-#SBATCH --output slurm.out
-#SBATCH --mem-per-cpu 1000
+#SBATCH --job-name=maxcut-benchmark
+#SBATCH --ntasks=20
+#SBATCH --cpus-per-task=4
+#SBATCH --output=slurm.out
+#SBATCH --mem-per-cpu=1000
 
 dir=$1
 exec_name='python3 ../src/python/batch.py'
@@ -18,10 +19,10 @@ do
     fname=`basename $file .$ext`
     fdir=`dirname $file`
     logfile="$fdir/$fname.csv"
-    echo "${exec_name} ${config_file} ${file}  > ${2}/${logfile}"
+    echo "${exec_name} --stdout ${config_file} ${file}  > ${2}/${logfile}"
     echo "${2}/${logfile}"
     install -D -m 644 /dev/null "${2}/${logfile}"
-    srun -n1 -c4 --exclusive --output "${2}/${logfile}" ${exec_name} --stdout ${config_file} ${file} &
+    srun -n1 --exclusive --output "${2}/${logfile}" ${exec_name} --stdout ${config_file} ${file} &
 done
 
 wait
