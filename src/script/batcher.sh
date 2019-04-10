09cf16357b53b7c684039ee8640920db7870d1f2
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2019-04-10 20:03:59 +0200

    update batcher for new cpp impl

--- src/script/batcher.sh
+++ src/script/batcher.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-dir=$1
-exec_name='python3 ../src/python/batch.py'
-config_file=$3
+thread_count=4
 
-echo $2
-install -d $2
+instance_dir=$1
+exec_name='./maxcut-benchmark'
+config_file=$2
 
-for file in `find -L $dir -type f`
+conf_ext=${config_file##*.}
+result_dir=`basename $config_file .$conf_ext`
+echo "using output directory: $result_dir"
+
+install -d $result_dir
+mv $config_file $result_dir
+config_file="$result_dir/$config_file"
+
+for file in `find -L $instance_dir -type f | grep -v .git`
 do
     ext=${file##*.}
     fname=`basename $file .$ext`
     fdir=`dirname $file`
     logfile="$fdir/$fname.csv"
-    cmd="${exec_name} --stdout ${config_file} ${file} > ${2}/${logfile}"
+    cmd="OMP_NUM_THREADS=${thread_count} ${exec_name} ${file} ${config_file} > $result_dir/${logfile}"
     echo $cmd
-    install -D -m 644 /dev/null "${2}/${logfile}"
-	eval $cmd
+    install -D -m 644 /dev/null "$result_dir/${logfile}"
+    eval $cmd
 done
