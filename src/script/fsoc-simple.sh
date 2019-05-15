cfca7e6953a396d84ca3dbaa4a2a4f3a04407f28
Soeren Tietboehl <soeren.tietboehl@student.hpi.uni-potsdam.de>
2019-05-15 17:06:50 +0200

    Update fsoc scripts
    Add possibility to run on multiple nodes

--- src/script/fsoc-simple.sh
+++ src/script/fsoc-simple.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-fsoc_processor_count=80
+fsoc_processor_count=50
 instance_dir=$1
 config=$2
 cpus=$3
+nodes=$4
 name=`basename $config .json`
 ((tasks = fsoc_processor_count / cpus))
 
-sbatch --cpus-per-task $cpus --job-name mxct-$name --ntasks $tasks ../src/script/fsoc.sh $instance_dir $config
+sbatch --nodes $nodes --cpus-per-task $cpus --job-name mxct-$name --ntasks-per-node $tasks ../src/script/fsoc.sh $instance_dir $config
 
--- src/script/fsoc.sh
+++ src/script/fsoc.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #SBATCH --job-name=maxcut-benchmark
-#SBATCH --ntasks=20
+#SBATCH --ntasks-per-node=12
 #SBATCH --cpus-per-task=4
 #SBATCH --output=slurm.out
 #SBATCH --mem-per-cpu=4000
 
 instance_dir=$1
-exec_name='python3 ../src/python/batch.py'
+exec_name='./maxcut-benchmark'
 config_file=$2
 
 conf_ext=${config_file##*.}
@@ -24,10 +24,10 @@ do
     fname=`basename $file .$ext`
     fdir=`dirname $file`
     logfile="$fdir/$fname.csv"
-    echo "${exec_name} --stdout ${config_file} ${file}  > $result_dir/${logfile}"
+    echo "${exec_name} ${file} ${config_file}  > $result_dir/${logfile}"
     echo "$result_dir/${logfile}"
     install -D -m 644 /dev/null "$result_dir/${logfile}"
-    srun -n1 --exclusive --output "$result_dir/${logfile}" ${exec_name} --stdout ${config_file} ${file} &
+    srun -n1 --exclusive --output "$result_dir/${logfile}" ${exec_name} ${file} ${config_file} &
 done
 
 wait
