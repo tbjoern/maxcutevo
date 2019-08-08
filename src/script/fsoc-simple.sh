f1804519203505de0cf85bbd8567f2ca807bf286
Soeren Tietboehl <soeren.tietboehl@student.hpi.uni-potsdam.de>
2019-08-08 11:40:54 +0200

    Revert "Remove exclusive from fsoc scripts"
    This reverts commit f56cfe789414647bdee8c84c5f447c103fb02c2f.

--- src/script/fsoc-simple.sh
+++ src/script/fsoc-simple.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-fsoc_processor_count=80
+fsoc_processor_count=50
 instance_dir=$1
 config=$2
 cpus=$3
@@ -8,5 +8,5 @@ nodes=$4
 name=`basename $config .json`
 ((tasks = fsoc_processor_count / cpus))
 
-sbatch --output $name.out --nodes $nodes --cpus-per-task $cpus --job-name mx-$name --ntasks-per-node $tasks ../src/script/fsoc.sh $instance_dir $config
+sbatch --nodes $nodes --cpus-per-task $cpus --job-name mx-$name --ntasks-per-node $tasks ../src/script/fsoc.sh $instance_dir $config
 
