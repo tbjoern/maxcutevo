f56cfe789414647bdee8c84c5f447c103fb02c2f
Soeren Tietboehl <soeren.tietboehl@student.hpi.uni-potsdam.de>
2019-08-08 11:38:15 +0200

    Remove exclusive from fsoc scripts

--- src/script/fsoc.sh
+++ src/script/fsoc.sh
@@ -2,7 +2,6 @@
 #SBATCH --job-name=maxcut-benchmark
 #SBATCH --ntasks-per-node=12
 #SBATCH --cpus-per-task=4
-#SBATCH --output=slurm.out
 #SBATCH --mem-per-cpu=4000
 
 instance_dir=$1
@@ -15,7 +14,8 @@ echo "using output directory: $result_dir"
 
 install -d $result_dir
 mv $config_file $result_dir
-config_file="$result_dir/$config_file"
+config_basename=`basename $config_file`
+config_file="$result_dir/$config_basename"
 
 for file in `find -L $instance_dir -type f | grep -v .git | grep -v .assignment`
 do
@@ -26,7 +26,7 @@ do
     echo "${exec_name} ${file} ${config_file}  > $result_dir/${logfile}"
     echo "$result_dir/${logfile}"
     install -D -m 644 /dev/null "$result_dir/${logfile}"
-    srun -n1 --output "$result_dir/${logfile}" ${exec_name} ${file} ${config_file} &
+    srun -N1 -n1 --output "$result_dir/${logfile}" ${exec_name} ${file} ${config_file} &
 done
 
 wait
